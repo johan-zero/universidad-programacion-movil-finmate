fecaa6bb11e5e981a4521a2006987d88adbbd1e8
Johan Sebastian Camilo Rodriguez <jozero9322@gmail.com>
2026-05-26 23:09:05 -0500

    chore: agregar configuración Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.edu.poligran.finmate',
+  appName: 'FinMate Mobile',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
