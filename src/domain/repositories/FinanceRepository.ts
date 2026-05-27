b3b71c1cf5b144ec40f3205360c76dac69d8bf8e
Johan Sebastian Camilo Rodriguez <jozero9322@gmail.com>
2026-05-26 23:17:40 -0500

    feat: definir contrato de repositorio financiero

---
+++ src/domain/repositories/FinanceRepository.ts
@@ -0,0 +1,6 @@
+import type { Transaction } from '../entities/Transaction';
+
+export interface FinanceRepository {
+  listTransactions(): Promise<Transaction[]>;
+  saveTransaction(transaction: Transaction): Promise<void>;
+}
