34447c492ee6b66c72b76dd0341a6a22773c024f
Johan Sebastian Camilo Rodriguez <jozero9322@gmail.com>
2026-06-23 19:18:14 -0500

    feat: conectar estado financiero de aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { IonApp, setupIonicReact } from '@ionic/react';
 import AppRoutes from './app/AppRoutes';
-import '@ionic/react/css/core.css';
+import { FinanceProvider } from './app/FinanceContext';
 import './theme/variables.css';
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/flex-utils.css';
 
 setupIonicReact();
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+const root = document.getElementById('root') as HTMLElement;
+createRoot(root).render(
   <React.StrictMode>
     <IonApp>
-      <AppRoutes />
+      <FinanceProvider><AppRoutes /></FinanceProvider>
     </IonApp>
   </React.StrictMode>
 );
