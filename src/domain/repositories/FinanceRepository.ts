c7a88e6b8bf240ecb79372e3a3c3177a35f628e0
Johan Sebastian Camilo Rodriguez <jozero9322@gmail.com>
2026-06-23 19:24:35 -0500

    refactor: completar contrato de repositorio

--- src/domain/repositories/FinanceRepository.ts
+++ src/domain/repositories/FinanceRepository.ts
@@ -3,4 +3,5 @@ import type { Transaction } from '../entities/Transaction';
 export interface FinanceRepository {
   listTransactions(): Promise<Transaction[]>;
   saveTransaction(transaction: Transaction): Promise<void>;
+  clear(): Promise<void>;
 }
