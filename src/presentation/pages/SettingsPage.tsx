976bbf7f3b45c32dc8bdd3080dafe27707a99da3
Johan Sebastian Camilo Rodriguez <jozero9322@gmail.com>
2026-06-23 19:22:23 -0500

    feat: configurar preferencias de usuario

--- src/presentation/pages/SettingsPage.tsx
+++ src/presentation/pages/SettingsPage.tsx
@@ -1,17 +1,21 @@
 import { IonContent, IonItem, IonLabel, IonList, IonPage, IonToggle } from '@ionic/react';
 import AppHeader from '../components/AppHeader';
 import BottomNav from '../components/BottomNav';
+import { useFinance } from '../../app/FinanceContext';
 
 export default function SettingsPage() {
+  const { settings, updateSettings } = useFinance();
   return (
     <IonPage>
       <AppHeader title="Ajustes" />
-      <IonContent className="ion-padding">
-        <IonList>
-          <IonItem><IonLabel>Alertas locales</IonLabel><IonToggle checked /></IonItem>
-          <IonItem><IonLabel>Modo oscuro</IonLabel><IonToggle /></IonItem>
-          <IonItem><IonLabel>Almacenamiento local standalone</IonLabel><IonToggle checked disabled /></IonItem>
-        </IonList>
+      <IonContent className="ion-padding finmate-content">
+        <section className="finmate-card">
+          <IonList lines="full">
+            <IonItem><IonLabel>Alertas locales</IonLabel><IonToggle checked={settings.alertsEnabled} onIonChange={(event) => updateSettings({ alertsEnabled: event.detail.checked })}/></IonItem>
+            <IonItem><IonLabel>Modo oscuro</IonLabel><IonToggle checked={settings.darkMode} onIonChange={(event) => updateSettings({ darkMode: event.detail.checked })}/></IonItem>
+            <IonItem><IonLabel>Almacenamiento local</IonLabel><IonToggle checked disabled /></IonItem>
+          </IonList>
+        </section>
       </IonContent>
       <BottomNav />
     </IonPage>
